c773663e2feedd8dff9cb7f554c819d6e7b6b8de
Wizley <wizley9999@gmail.com>
2025-11-06 18:18:34 +0900

    Update firebase configs

--- functions/config/firebase.js
+++ functions/config/firebase.js
@@ -1,8 +1,11 @@
 import { initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getMessaging } from "firebase-admin/messaging";
+import { setGlobalOptions } from "firebase-functions";
 
 initializeApp();
+
+setGlobalOptions({ region: "asia-northeast3" });
 
 export const firestore = getFirestore();
 export const messaging = getMessaging();
